6ecab462a0827406e4ec3e43c42bfe8cfcb7ae43
Sidharthan Chandrasekaran Kamaraj <csidharthank@gmail.com>
2020-07-07 17:57:54 +0100

    Heroku Settings

--- twitter_sentiment_webapp/settings.py
+++ twitter_sentiment_webapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -31,7 +32,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.staticfiles',
     'home',
-    'herokuapp',
 ]
 
 MIDDLEWARE = [
@@ -100,3 +100,4 @@ GOOGLE_MAPS_API_KEY = os.environ.get('GOOGLE_MAPS_API_KEY')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+django_heroku.settings(locals())
